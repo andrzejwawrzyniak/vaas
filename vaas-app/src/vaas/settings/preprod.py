5075dd433578e0a6adcb5c87185387501e0d2a95
Marcin Zaremba <marcin.zaremba@allegrogroup.com>
2016-01-04 14:10:24 +0100

    Add preprod configuration environment

---
+++ vaas-app/src/vaas/settings/preprod.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from .base import *
+from vaas.configuration.loader import YamlConfigLoader
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = ["*"]
+
+# override some configurations
+for key, value in YamlConfigLoader().get_config_tree('preprod.yml').iteritems():
+    globals()[key] = value
